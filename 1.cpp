ae06a01578f4e787fac6f74b01c1087c3cf6800c
zhangzk0313 <815028387@qq.com>
2020-02-25 20:39:28 +0800

    my first CPrograme

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author: zhang zk
+	> Mail: 815028387@qq.com 
+	> Created Time: 2020年02月25日 星期二 20时34分06秒
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
